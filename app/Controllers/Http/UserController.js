0141d90d1684cc49ae0352196c3f7a0654e42a81
Sterczik <sterczewskik@gmail.com>
2019-03-20 21:52:12 +0100

    Express to Adonis - Fixes in UserController

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -2,6 +2,7 @@
 
 const HTTPStatus = require('http-status')
 const User = use('App/Models/User')
+const Hash = use('Hash')
 const { validate } = use('Validator')
 
 class UserController {
@@ -18,10 +19,8 @@ class UserController {
               type: 'email',
               message: 'This email already exists'
             }
-          });
+          })
       }
-
-      //! Get registerValidationSchema from express
 
       const validation = await validate(request.only(['name', 'email', 'password', 'password_confirmation']), {
         email: 'required|email|unique:users',
@@ -36,7 +35,7 @@ class UserController {
           errors: {
             message: validation.messages()
           }
-        });
+        })
       }
 
       const user = await User.create(data)
@@ -48,7 +47,7 @@ class UserController {
           message: 'You successfully registered.'
         })
     } catch(err) {
-      return response.status(500).json({
+      return response.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
         status: 'error',
         message: 'Problem occured while trying to signup. Please try again.'
       })
@@ -59,19 +58,56 @@ class UserController {
     try {
       const data = request.only(['email', 'password'])
 
-      if (!data) {
-        return response.status(404).json({ data: 'Resource not found' })
+      const validation = await validate(request.only(['email', 'password']), {
+        email: 'required|email',
+        password: 'required|min:6|max:30'
+      })
+      //|regex:/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/
+
+      if (validation.fails()) {
+        return response.status(HTTPStatus.BAD_REQUEST).json({
+          success: false,
+          errors: {
+            message: validation.messages()
+          }
+        })
+      }
+
+      const userExists = await User.findBy('email', data.email)
+
+      if (!userExists) {
+        return response.status(HTTPStatus.BAD_REQUEST)
+          .json({
+            success: false,
+            errors: {
+              message: 'There is no account with this email.'
+            }
+          })
+      }
+
+      const passwordCheck = await Hash.verify(data.password, userExists.password)
+
+      if (!passwordCheck) {
+        return response.status(HTTPStatus.BAD_REQUEST)
+          .json({
+            success: false,
+            errors: {
+              message: 'Invalid password'
+            }
+          })
       }
 
       const token = await auth
         .withRefreshToken()
         .attempt(data.email, data.password)
 
-      return response.json({
-        token
-      })
+      return response.status(HTTPStatus.OK)
+        .json({
+          success: true,
+          token
+        })
     } catch(err) {
-      return response.status(400).json({
+      return response.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
         status: 'error',
         message: 'Problem occured while trying to sigin. Please try again.'
       })
